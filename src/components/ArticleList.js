fb57f60cc670b8910b686726a5d3be626fd48591
Eric Krukowski <erickruk@amazon.com>
2023-05-28 19:10:33 -0400

    Lab complete

---
+++ src/components/ArticleList.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Article from "./Article"
+
+function ArticleList({posts}){
+    const postArr = posts.map(post=>{
+        return <Article key={post.id} title={post.title} date={post.date} preview={post.preview} minutes={post.minutes} />
+    })
+    return (
+        <main>
+            {postArr}
+        </main>
+    )
+}
+
+export default ArticleList
